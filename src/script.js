8c4a5fa13bf8238bc49caf4623b35794f4ee0c0b
milano5naranja <120511247+milano5naranja@users.noreply.github.com>
2023-06-08 19:39:56 +0100

    Making changes to Javascript

--- src/script.js
+++ src/script.js
@@ -1,8 +1,53 @@
-function showWeather(response) {
-  let h1 = document.querySelector("h1");
-  let temperature = Math.round(response.data.main.temp);
-  //h1.innerHTML = `It is currently ${temperature}° in ${response.data.name}`;
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
+
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+}
+
+let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Helsinki&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
+
+//function showWeather(response) {
+//let h1 = document.querySelector("h1");
+//let temperature = Math.round(response.data.main.temp);
+//h1.innerHTML = `It is currently ${temperature}° in ${response.data.name}`;
+//}
 
 //function retrievePosition(position) {
 //let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
@@ -18,88 +63,58 @@ function showWeather(response) {
 
 //navigator.geolocation.getCurrentPosition(retrievePosition);
 
-function formatDate(date) {
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
+//let dayIndex = date.getDay();
 
-  let dayIndex = date.getDay();
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let day = days[dayIndex];
+//function displayWeatherCondition(response) {
+//document.querySelector("#city").innerHTML = response.data.name;
+//document.querySelector("#temperature").innerHTML = Math.round(
+//response.data.main.temp
+//);
 
-  return `${day} ${hours}:${minutes}`;
-}
+//document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+//document.querySelector("#wind").innerHTML = Math.round(
+//response.data.wind.speed
+//);
+//document.querySelector("#description").innerHTML =
+//response.data.weather[0].main;
+//}
+//function searchCity(city) {
 
-function displayWeatherCondition(response) {
-  document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.main.temp
-  );
+//function handleSubmit(event) {
+//event.preventDefault();
+//let city = document.querySelector("#city-input").value;
+//searchCity(city);
+//}
 
-  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#wind").innerHTML = Math.round(
-    response.data.wind.speed
-  );
-  document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
-}
-function searchCity(city) {
-  let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+//function searchLocation(position) {
+//axios.get(apiUrl).then(displayWeatherCondition);
+//}
 
-  axios.get(apiUrl).then(displayWeatherCondition);
-}
+//function getCurrentLocation(event) {
+//event.preventDefault();
+//navigator.geolocation.getCurrentPosition(searchLocation);
+//}
 
-function handleSubmit(event) {
-  event.preventDefault();
-  let city = document.querySelector("#city-input").value;
-  searchCity(city);
-}
+//function convertToFahrenheit(event) {
+// event.preventDefault();
+//let temperatureElement = document.querySelector("#temperature");
+//temperatureElement.innerHTML = 66;
+//}
 
-function searchLocation(position) {
-  let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayWeatherCondition);
-}
+//function convertToCelsius(event) {
+//event.preventDefault();
+//let temperatureElement = document.querySelector("#temperature");
+//temperatureElement.innerHTML = 19;
+//}
 
-function getCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
-}
+//let dateElement = document.querySelector("#date");
+//let currentTime = new Date();
+//dateElement.innerHTML = formatDate(currentTime);
 
-function convertToFahrenheit(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = 66;
-}
+//let searchForm = document.querySelector("#search-form");
+//searchForm.addEventListener("submit", handleSubmit);
 
-function convertToCelsius(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = 19;
-}
+//let currenLocationButton = document.querySelector("#current-location-button");
+//currenLocationButton.addEventListener("click", getCurrentLocation);
 
-let dateElement = document.querySelector("#date");
-let currentTime = new Date();
-dateElement.innerHTML = formatDate(currentTime);
-
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", handleSubmit);
-
-let currenLocationButton = document.querySelector("#current-location-button");
-currenLocationButton.addEventListener("click", getCurrentLocation);
-
-searchCity("Helsinki");
+//searchCity("Helsinki");
